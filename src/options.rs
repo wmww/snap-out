2161912be168c76dce44496a5a0a4e1016fbcde2
William Wold <wm@wmww.sh>
2019-06-29 00:19:03 +0000

    Add tests for option parsing

--- src/options.rs
+++ src/options.rs
@@ -37,6 +37,7 @@ Options:
     );
 }
 
+#[derive(Debug, PartialEq)]
 enum Parsed {
     RunCommand { command: String, args: Vec<String> },
     ShowHelp,
@@ -45,10 +46,10 @@ enum Parsed {
     None,
 }
 
-fn parse(args: impl std::iter::Iterator<Item = String>) -> Parsed {
+fn parse(args: impl std::iter::Iterator<Item = impl AsRef<str>>) -> Parsed {
     // skip the first arg, as it is just the current program
     let mut args = args.skip(1);
-    let command: Option<String> = args.next();
+    let command: Option<String> = args.next().map(|s| String::from(s.as_ref()));
     if let Some(command) = command {
         if command.starts_with("-") {
             if command == "--help" || command == "-h" {
@@ -61,10 +62,85 @@ fn parse(args: impl std::iter::Iterator<Item = String>) -> Parsed {
         } else {
             Parsed::RunCommand {
                 command,
-                args: args.collect(),
+                args: args.map(|s| String::from(s.as_ref())).collect(),
             }
         }
     } else {
         Parsed::None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parses_command_with_no_args() {
+        assert_eq!(
+            parse(vec!["snap-out", "ls"].iter()),
+            Parsed::RunCommand{
+                command: "ls".to_owned(),
+                args: vec![],
+            }
+        );
+    }
+
+    #[test]
+    fn parses_command_with_args() {
+        assert_eq!(
+            parse(vec!["snap-out", "ls", "..", "-a"].iter()),
+            Parsed::RunCommand{
+                command: "ls".to_owned(),
+                args: vec!["..", "-a"].iter().map(|s| s.to_string()).collect(),
+            }
+        );
+    }
+
+    #[test]
+    fn parses_show_help() {
+        assert_eq!(
+            parse(vec!["snap-out", "--help"].iter()),
+            Parsed::ShowHelp,
+        );
+        assert_eq!(
+            parse(vec!["snap-out", "-h"].iter()),
+            Parsed::ShowHelp,
+        );
+    }
+
+    #[test]
+    fn parses_show_version() {
+        assert_eq!(
+            parse(vec!["snap-out", "--version"].iter()),
+            Parsed::ShowVersion,
+        );
+        assert_eq!(
+            parse(vec!["snap-out", "-v"].iter()),
+            Parsed::ShowVersion,
+        );
+    }
+
+    #[test]
+    fn errors_on_bad_arg() {
+        match parse(vec!["snap-out", "--bad"].iter()) {
+            Parsed::Error(_) => (),
+            result @ _ => panic!("Should have been an error, but instead returned {:?}", result),
+        };
+        match parse(vec!["snap-out", "-x"].iter()) {
+            Parsed::Error(_) => (),
+            result @ _ => panic!("Should have been an error, but instead returned {:?}", result),
+        };
+    }
+
+    #[test]
+    fn none_on_no_args() {
+        assert_eq!(
+            parse(vec!["snap-out"].iter()),
+            Parsed::None,
+        );
+        assert_eq!(
+            parse((vec![] as Vec<&str>).iter()),
+            Parsed::None,
+        );
     }
 }
