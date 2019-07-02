66e9fa08398c518934298f8e319e514281471c5d
William Wold <wm@wmww.sh>
2019-07-02 17:31:53 +0000

    Move option handling to main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,21 @@ mod variable;
 use std::error::Error;
 use std::ffi::OsString;
 
+fn get_help_text() -> String {
+    return format!(
+        "Usage: {0} [COMMAND] [ARGUMENTS]...
+       {0} [OPTION]
+
+{1}
+
+Options:
+  -h, --help        Print this help message and exit
+  -v, --version     Print the version and exit",
+        env!("CARGO_PKG_NAME"),
+        env!("CARGO_PKG_DESCRIPTION")
+    );
+}
+
 fn get_variables() -> Result<Vec<(OsString, Option<OsString>)>, Box<Error>> {
     let process = process::ProcfsProcess::myself()?;
     let environments = environments::All::detect(&process)?;
@@ -21,15 +36,42 @@ fn get_variables() -> Result<Vec<(OsString, Option<OsString>)>, Box<Error>> {
     Ok(vars)
 }
 
-fn main() {
-    let (cmd, args) = options::handle();
-    let exit_code = match get_variables() {
-        Ok(vars) => command::run(&cmd, args, vars),
+fn run_command_outside_snap(
+    cmd: &str,
+    args: impl IntoIterator<Item = impl AsRef<std::ffi::OsStr>>,
+) -> i32 {
+    match get_variables() {
+        Ok(vars) => command::run(cmd, args, vars),
         Err(e) => {
             eprintln!("snap-out: {}, running in unmodified environment", e);
             let no_vars: Vec<(&str, Option<&str>)> = Vec::new();
-            command::run(&cmd, args, no_vars)
+            command::run(cmd, args, no_vars)
         }
-    };
-    std::process::exit(exit_code);
+    }
+}
+
+fn main() {
+    let parsed = options::parse(std::env::args());
+    match parsed {
+        options::RunCommand { command, args } => {
+            let exit_code = run_command_outside_snap(&command, args);
+            std::process::exit(exit_code);
+        }
+        options::ShowHelp => {
+            println!("{}", get_help_text());
+            std::process::exit(0);
+        }
+        options::ShowVersion => {
+            println!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+            std::process::exit(0);
+        }
+        options::Error(e) => {
+            eprintln!("Error parsing arguments: {}", e);
+            std::process::exit(1);
+        }
+        options::None => {
+            eprintln!("No command to run, use --help for help");
+            std::process::exit(0);
+        }
+    }
 }
--- src/options.rs
+++ src/options.rs
@@ -1,44 +1,7 @@
-/// In special cases (such as --help) it handles them and exits
-/// In the normal case it returns the command + list-of-arguments that were sent in
-pub fn handle() -> (String, Vec<String>) {
-    match parse(std::env::args()) {
-        Parsed::RunCommand { command, args } => (command, args),
-        Parsed::ShowHelp => {
-            println!("{}", get_help_text());
-            std::process::exit(0);
-        }
-        Parsed::ShowVersion => {
-            println!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
-            std::process::exit(0);
-        }
-        Parsed::Error(e) => {
-            eprintln!("Error parsing arguments: {}", e);
-            std::process::exit(1);
-        }
-        Parsed::None => {
-            eprintln!("No command to run, use --help for help");
-            std::process::exit(0);
-        }
-    }
-}
-
-fn get_help_text() -> String {
-    return format!(
-        "Usage: {0} [COMMAND] [ARGUMENTS]...
-
-       {0} [OPTION]
-{1}
-
-Options:
-  -h, --help        Print this help message and exit
-  -v, --version     Print the version and exit",
-        env!("CARGO_PKG_NAME"),
-        env!("CARGO_PKG_DESCRIPTION")
-    );
-}
+pub use Parsed::*;
 
 #[derive(Debug, PartialEq)]
-enum Parsed {
+pub enum Parsed {
     RunCommand { command: String, args: Vec<String> },
     ShowHelp,
     ShowVersion,
@@ -46,7 +9,7 @@ enum Parsed {
     None,
 }
 
-fn parse(args: impl std::iter::Iterator<Item = impl AsRef<str>>) -> Parsed {
+pub fn parse(args: impl std::iter::Iterator<Item = impl AsRef<str>>) -> Parsed {
     // skip the first arg, as it is just the current program
     let mut args = args.skip(1);
     let command: Option<String> = args.next().map(|s| String::from(s.as_ref()));
